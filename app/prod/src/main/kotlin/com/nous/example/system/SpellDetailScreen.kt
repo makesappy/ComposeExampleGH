c56a25644f672dd6d66736ea44ae8a0e63f50c96
Heorhii <Karpenko>
2022-12-01 00:08:55 +0100

    fix text alignment

--- app/prod/src/main/kotlin/com/nous/example/system/SpellDetailScreen.kt
+++ app/prod/src/main/kotlin/com/nous/example/system/SpellDetailScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -85,6 +86,7 @@ private fun SpellDetailScreenImpl(
                             start = CustomTheme.dimensions.spaceXXL,
                             end = CustomTheme.dimensions.spaceXXL
                         ),
+                        textAlign = TextAlign.Center,
                         text = description.uppercase(),
                         color = customLightColors().backgroundPrimary,
                         style = CustomTheme.typography.body1
